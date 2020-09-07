75bbb8389552c0d7442b2de97d23866e209077e2
mitjko <mitjkoz@gmail.com>
2020-09-07 20:39:03 +0200

    working with @PropertySource

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,51 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+
+@Configuration
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${guru.username}")
+    String username;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        return new FakeDataSource(env.getProperty("USERNAME"), password, url);
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        return new FakeJmsBroker(jmsUsername, jmsPassword, jmsUrl);
+    }
+}
---
+++ src/main/java/guru/springframework/examplebeans/FakeDataSource.java
@@ -0,0 +1,38 @@
+package guru.springframework.examplebeans;
+
+public class FakeDataSource {
+
+    private String username;
+    private String password;
+    private String url;
+
+    public FakeDataSource(String user, String password, String url) {
+        this.username = user;
+        this.password = password;
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
